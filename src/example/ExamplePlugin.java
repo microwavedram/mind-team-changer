3201e5ba185017a5abf14fbd12f379b5b122a5c0
microwavedram <89029844+microwavedram@users.noreply.github.com>
2022-09-25 15:20:16 +0100

    first & only

--- src/example/ExamplePlugin.java
+++ src/example/ExamplePlugin.java
@@ -5,6 +5,7 @@ import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
+import mindustry.game.Team;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.net.Administration.*;
@@ -12,48 +13,18 @@ import mindustry.world.blocks.storage.*;
 
 public class ExamplePlugin extends Plugin{
 
+    public boolean teamsLocked = false;
+
     //called when game initializes
     @Override
     public void init(){
-        //listen for a block selection event
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
-                //player is the unit controller
-                Player player = event.builder.getPlayer();
-
-                //send a message to everyone saying that this player has begun building a reactor
-                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
-            }
-        });
-
-        //add a chat filter that changes the contents of all messages
-        //in this case, all instances of "heck" are censored
-        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
-
-        //add an action filter for preventing players from doing certain things
-        Vars.netServer.admins.addActionFilter(action -> {
-            //random example: prevent blast compound depositing
-            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
-                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
-                return false;
-            }
-            return true;
-        });
+        Call.sendMessage("[scarlet] hi guys");
     }
 
-    //register commands that run on the server
     @Override
     public void registerServerCommands(CommandHandler handler){
-        handler.register("reactors", "List all thorium reactors in the map.", args -> {
-            for(int x = 0; x < Vars.world.width(); x++){
-                for(int y = 0; y < Vars.world.height(); y++){
-                    //loop through and log all found reactors
-                    //make sure to only log reactor centers
-                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor && Vars.world.tile(x, y).isCenter()){
-                        Log.info("Reactor at @, @", x, y);
-                    }
-                }
-            }
+        handler.register("teamlock", "Toggle Team Lock", args -> {
+            teamsLocked = !teamsLocked;
         });
     }
 
@@ -62,23 +33,45 @@ public class ExamplePlugin extends Plugin{
     public void registerClientCommands(CommandHandler handler){
 
         //register a simple reply command
-        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
-            player.sendMessage("You said: [accent] " + args[0]);
-        });
+        handler.<Player>register("team", "<team>", "A command to change your team", (args, player) -> {
 
-        //register a whisper command which can be used to send other players messages
-        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
-            //find player by name
-            Player other = Groups.player.find(p -> p.name.equalsIgnoreCase(args[0]));
-
-            //give error message with scarlet-colored text if player isn't found
-            if(other == null){
-                player.sendMessage("[scarlet]No player by that name found!");
+            if (teamsLocked) {
+                player.sendMessage("[scarlet] Teams are currently locked");
                 return;
             }
 
-            //send the other player a message, using [lightgray] for gray text color and [] to reset color
-            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+            Team target;
+
+            switch (args[0].toLowerCase()){
+                case "derelict":
+                case "black":
+                    target = Team.derelict;
+                    break;
+                case "sharded":
+                case "yellow":
+                    target = Team.sharded;
+                    break;
+                case "crux":
+                case "red":
+                    target = Team.crux;
+                    break;
+                case "malis":
+                case "purple":
+                    target = Team.purple;
+                    break;
+                case "green":
+                    target = Team.green;
+                    break;
+                case "blue":
+                    target = Team.blue;
+                    break;
+                default:
+                    player.sendMessage("[scarlet] No team with that name found");
+                    return;
+            }
+
+            player.team(target);
         });
     }
 }
+
